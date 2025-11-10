04077d14dfc269184483a80f024f09d27f37e8ed
Shuting Wang <58683838+Julylle@users.noreply.github.com>
2025-11-10 14:32:02 +1000

    Add files via upload

--- demo.py
+++ demo.py
@@ -4,15 +4,11 @@ Created on Mon Nov 10 13:20:01 2025
 
 @author: Shuting Wang
 """
-
-import sys
-from pathlib import Path
-
-# add src folder to sys.path (so Python can see it)
-sys.path.append(str(Path(__file__).resolve().parent / "src"))
-
+#%%
 from liquidbased_quantification import LiquidQuantifier
 
-# now run
 model = LiquidQuantifier("data/Monitoring_Data_Example.xlsx")
 results = model.run_pipeline(show_plots=True)
+print(results.head())
+results.to_excel("liquid_quantification_results.xlsx", index=False)
+
